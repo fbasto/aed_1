bdebd228a840ee9524a47eaa743dd39aee260d6b
fbasto <fbasto00@gmail.com>
2017-03-19 20:03:21 +0000

    ainda nao funciona

--- avltree.py
+++ avltree.py
@@ -1,16 +1,19 @@
 import csv
+import time
 outputdebug = False 
 
 def debug(msg):
     if outputdebug:
         print (msg)
 
-class Node():
+class Node:
     def __init__(self, init_ctry_name, init_ctry_code):
       self.ctry_name = init_ctry_name
       self.ctry_code = init_ctry_code
       self.ctry_pop = {} 
       self.height=0   
+      for i in range(0,57):
+         self.ctry_pop[1960+i] = None
       self.rightChild= None
       self.leftChild=None
 
@@ -19,19 +22,21 @@ class Node():
     def get_ctry_code(self):
       return self.ctry_code
     def get_next(self):
-         return self.next
+        return self.next
+    def get_ctry_pop(self):
+      return self.ctry_pop
     def set_ctry_name(self, new_ctry_name):
       self.ctry_name = newctry_name
     def set_ctry_code(self, new_ctry_code):
       self.ctry_code = newctry_code
     def set_next(self, new_next):
       self.next = new_next   
-    def print(self)
-      print(,end='')
-    
+    def nodeprint(self):
+       print(self.ctry_name,self.ctry_code,end=' ')
+       print(self.ctry_pop)
 
 
-class AVLTree():
+class AVLTree:
     def __init__(self, *args):
        self.node = None
        self.height = -1
@@ -50,20 +55,18 @@ class AVLTree():
     def insert(self,init_ctry_name, init_ctry_code):
         tree = self.node
 
-        newnode = Node(ctry_name, init_ctry_code)
+        newnode = Node(init_ctry_name, init_ctry_code)
 
         if tree == None:
             self.node = newnode
             self.node.leftChild = AVLTree()
             self.node.rightChild = AVLTree()
+            return self.node
 
-
-        elif ctry_name < tree.ctry_name:
-            self.node.leftChild.insert(ctry_name, init_ctry_code)
-        elif ctry_name < tree.ctry_name:
-            self.node.leftChild.insert(ctry_name, init_ctry_code)
-        else: 
-            debug("Key [" + str(key) + "] already in tree.")
+        elif init_ctry_name < tree.ctry_name:
+            self.node.leftChild.insert(init_ctry_name, init_ctry_code)
+        elif init_ctry_name < tree.ctry_name:
+            self.node.leftChild.insert(init_ctry_name, init_ctry_code)
 
         self.rebalance()
 
@@ -136,23 +139,34 @@ class AVLTree():
 
     def remove(self,ctry_name):
         if self.node != None:
-            if self.node.ctry_name
+            if self.node.ctry_name == ctry_name:
+                if self.node.leftChild.node == None and self.node.rightChild.node == None:
+                    self.node = None
+                elif self.node.leftChild.node == None:
+                    self.node = self.node.rightChild.node
+                elif self.node.rightChild.node == None:
+                    self.node = self.node.leftChild.node
+
+                else:
+                    ŕeplacement = self.logical_successor(self.node)
+                    if replacement != None:
+                        self.node.ctry_name = replacement.ctry_name
+
+                        self.node.rightChild.remove(replacement.ctry_name)
+                
+                self.rebalance()
+                return
+            elif ctry_name < self.node.ctry_name:
+                self.node.leftChild.remove(ctry_name)
+            elif ctry_name > self.node.ctry_name:
+                self.node.rightChild.remove(ctry_name)
+
+            self.rebalance()
+        else:
+            return
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-     def logical_predecessor(self, node):
-        
+    def logical_predecessor(self,node):
         node = node.leftChild.node 
         if node != None: 
             while node.rightChild != None:
@@ -165,8 +179,7 @@ class AVLTree():
 
 
 
-    def logical_successor(self, node):
-        
+    def logical_successor(self,node):
         node = node.rightChild.node  
         if node != None: # just a sanity check  
             
@@ -206,18 +219,108 @@ class AVLTree():
 
 
 
-    def search(self,ctry_name):
+    def find(self,ctry_name):
+        if self.node == None:
+            return
+        elif ctry_name < self.node.ctry_name:
+            self.node.leftChild.find(ctry_name)
+        elif ctry_name > self.node.ctry_name:
+            self.node.rightChild.find(ctry_name) 
+        elif self.node.ctry_name == ctry_name:
+            return self.node
 
 
 
 
+    def carregarDados(self):
+      with open('dados.csv', newline='') as csvfile:
+         spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+         for row in spamreader:
+            #Cada row = Cada país
+            row=', '.join(row)
+            row=row.split(';')
+            aux = t.insert(row[0],row[1])
+            for n in range(2,len(row)):
+               #Cada n = index de celula de pops
+               aux.get_ctry_pop()[1960+n-2] = row[n]
 
 
 
+if __name__ == "__main__": 
+   t = AVLTree()
+   timer = False
+   t.carregarDados()
+   while(True):
+      print("\nTemporizador de operações:",timer)
+      userop = eval(input("1-Pesquisa\n2-Insercao\n3-Edicao\n4-Remocao\n5-Fechar\n6-Ligar/Desligar temporizador de operações\n"))
+      if(userop == 1):  #PESQUISA
+         usercrit = eval(input("1-Pesquisa por nome\n2-Pesquisa por sigla\n"))
+         usertext = input("Inserir palavra: ")
+         start=time.time()
+         if(usercrit == 1):
+            aux = t.find(usertext)
+         if(usercrit == 2):
+            aux = t.findCode(usertext)
+         if(aux != None):
+            aux.nodeprint()
+         if(timer is True):
+            end=time.time()
+            print("Operacao demorou: %.10f segundos" %(end-start))
+
+      if(userop == 2):  #INSERCAO
+         # usercrit = eval(input("Pretende inserir todas as percentagem da populacao portuguesa com acesso a rede eletrica? 1-Sim 2-Não"))
+         usertext = input("Indicar nome de país a inserir: ")
+         usertext2 = input("Indicar codigo de país a inserir: ")
+         start=time.time()
+         t.insert(usertext,usertext2)
+         # if(usercrit == 1):
+         #    aux = t.insert(usertext,usertext2)
+         #    for n in range(0,57):
+         #       aux.get_ctry_pop()[1960+n] = input("Percentagem populacao em ",1960+n,": ")
+         # if(usercrit == 2):
+         #    t.insert(usertext,usertext2)
+         if(timer is True):
+            end=time.time()
+            print("Operacao demorou: %.10f segundos" %(end-start))
+
+      if(userop == 3):  #EDICAO  DE PERCENTAGEM
+         usertext = input("Indicar nome do país de que se pretende alterar a percentagem: ")
+         usertext2 = eval(input("Indicar ano que se pretende alterar (1960 a 2016 inclusive): "))
+         usertext3 = eval(input("Indicar valor: "))
+         start=time.time()
+         aux = t.find(usertext)
+         if(aux != None):
+            aux.get_ctry_pop()[usertext2] = usertext3
+         if(timer is True):
+            end=time.time()  
+            print("Operacao demorou: %.10f segundos" %(end-start))
+
+      if(userop == 4):  #REMOCAO
+         usercrit = eval(input("1-Remover país da lista\n2-Remover percentagem de um país-ano\n"))
+         if(usercrit == 1):
+            usertext = input("Indicar nome de país que se pretende remover: ")
+            start=time.time()
+            t.remove(usertext)
+            if(timer is True):
+               end=time.time()
+               print("Operacao demorou: %.10f segundos" %(end-start))
+         if(usercrit == 2):
+            usertext = input("Indicar nome do país do qual se pretende remover uma percentagem: ")
+            usertext2 = eval(input("Indicar o ano do qual se pretende remover uma percentagem: "))
+            start=time.time()
+            aux = t.find(usertext)
+            if(aux != None):
+               aux.get_ctry_pop()[usertext2] = None
+            if(timer is True):
+               end=time.time()
+               print("Operacao demorou: %.10f segundos" %(end-start))
+      if(userop == 5):
+         break
+      if(userop == 6):
+         if(timer is False):
+            timer = True
+         else:
+            timer = False
 
 
-
-if __name__ == '__main__':
-
-    #gerar uma arvore
-    a = AVLTree()
+   
