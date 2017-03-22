27051dff0c6f8cc93bdf90c9cfaf9d24d2eac6e6
JKiller13 <joaocorreiacampos@gmail.com>
2017-03-22 02:33:03 +0000

    bts tudo a dar bem

--- bts.py
+++ bts.py
@@ -60,15 +60,16 @@ class BinaryTree():
             self.rightChild.add(init_ctry_name,init_ctry_code)
       else:
          print("Nó já existe na árvore!")
+      return no
 
 
    #descobre o menor elemento da subarvore direita, copia os dados para o nó atual e é removido
    def remove(self,ctry_name):
-      if ctry_name < self.ctry_name:
+      if ctry_name < self.ctry_name and self.leftChild != None:
          self.leftChild = self.leftChild.remove(ctry_name)
-      elif ctry_name > self.ctry_name:
+      elif ctry_name > self.ctry_name and self.rightChild != None:
          self.rightChild = self.rightChild.remove(ctry_name)
-      else:
+      elif ctry_name == self.ctry_name:
          if self.rightChild is None:
             return self.leftChild
          if self.leftChild is None:
@@ -78,6 +79,9 @@ class BinaryTree():
          self.ctry_code=aux.ctry_code
          self.ctry_pop=aux.ctry_pop
          self.rightChild = self.rightChild.remove_min()
+      else: 
+         print("Nó não existe!")
+         return
       return self
 
    def find_min(self):
@@ -101,24 +105,12 @@ class BinaryTree():
             #Cada row = Cada país
             row=', '.join(row)
             row=row.split(';')
-            print(row[0],row[1])
             aux = self.add(row[0],row[1])
+            no = self.find(row[0])
             for n in range(2,len(row)):
                #Cada n = index de celula de pops
-               aux.get_ctry_pop()[1960+n-2] = row[n]
-            #print(aux.ctry_name,'!!',aux.ctry_code)
-            #print(aux.get_ctry_pop())
+               no.get_ctry_pop()[1960+n-2] = row[n]
 
-   def casoTeste(bts):
-      bts.add("Austria","AT")
-      bts.add("Portugal","PT")
-      bts.add("Espana","ET")
-      bts.add("se","dT")
-      bts.add("fs","sT")
-      bts.add("Alemanha","al")
-      bts.remove("Nome")
-      #bts.remove("s")
-      bts.printTree()
 
    def printTree(self):
       if self == None:
@@ -133,8 +125,78 @@ class BinaryTree():
 
 if __name__ == "__main__": 
    bts = BinaryTree("Nome","Código")
-  
-   bts.casoTeste()
+   timer = False
+   bts.carregarDados()
+   bts.remove("Nome")
+
+   bts.printTree()
+   while(True):
+      print("\nTemporizador de operações:",timer)
+      userop = eval(input("1-Pesquisa\n2-Insercao\n3-Edicao\n4-Remocao\n5-Fechar\n6-Ligar/Desligar temporizador de operações\n"))
+      if(userop == 1):  #PESQUISA
+         usercrit = eval(input("1-Pesquisa por nome\n2-Pesquisa por sigla\n"))
+         usertext = input("Inserir palavra: ")
+         start=time.time()
+         if(usercrit == 1):
+            aux = bts.find(usertext)
+         if(usercrit == 2):
+            aux = bts.findCode(usertext)
+         if(aux != None):
+            aux.nodeprint()
+         else:
+            print("Nao existe")
+         if(timer is True):
+            end=time.time()
+            print("Operacao demorou: %.10f segundos" %(end-start))
+
+      if(userop == 2):  #INSERCAO
+         # usercrit = eval(input("Pretende inserir todas as percentagem da populacao portuguesa com acesso a rede eletrica? 1-Sim 2-Não"))
+         usertext = input("Indicar nome de país a inserir: ")
+         usertext2 = input("Indicar codigo de país a inserir: ")
+         start=time.time()
+         bts.add(usertext,usertext2)
+         if(timer is True):
+            end=time.time()
+            print("Operacao demorou: %.10f segundos" %(end-start))
+
+      if(userop == 3):  #EDICAO  DE PERCENTAGEM
+         usertext = input("Indicar nome do país de que se pretende alterar a percentagem: ")
+         usertext2 = eval(input("Indicar ano que se pretende alterar (1960 a 2016 inclusive): "))
+         usertext3 = eval(input("Indicar valor: "))
+         start=time.time()
+         aux = bts.find(usertext)
+         if(aux != None):
+            aux.get_ctry_pop()[usertext2] = usertext3
+         if(timer is True):
+            end=time.time()  
+            print("Operacao demorou: %.10f segundos" %(end-start))
+
+      if(userop == 4):  #REMOCAO
+         usercrit = eval(input("1-Remover país da lista\n2-Remover percentagem de um país-ano\n"))
+         if(usercrit == 1):
+            usertext = input("Indicar nome de país que se pretende remover: ")
+            start=time.time()
+            bts.remove(usertext)
+            if(timer is True):
+               end=time.time()
+               print("Operacao demorou: %.10f segundos" %(end-start))
+         if(usercrit == 2):
+            usertext = input("Indicar nome do país do qual se pretende remover uma percentagem: ")
+            usertext2 = eval(input("Indicar o ano do qual se pretende remover uma percentagem: "))
+            start=time.time()
+            aux = bts.find(usertext)
+            if(aux != None):
+               aux.get_ctry_pop()[usertext2] = None
+            if(timer is True):
+               end=time.time()
+               print("Operacao demorou: %.10f segundos" %(end-start))
+      if(userop == 5):
+         break
+      if(userop == 6):
+         if(timer is False):
+            timer = True
+         else:
+            timer = False
 
    
    
